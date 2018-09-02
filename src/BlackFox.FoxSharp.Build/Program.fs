de9321ce7fd76627e379085486eb27081f408a01
Julien Roncaglia <julien@roncaglia.fr>
2018-09-02 21:43:00 +0200

    Use setupContextFromArgv

--- src/BlackFox.FoxSharp.Build/Program.fs
+++ src/BlackFox.FoxSharp.Build/Program.fs
@@ -4,18 +4,9 @@ open BlackFox.Fake
 open Fake.Core
 open Fake.BuildServer
 
-let setupFakeContext (argv: string list) =
-    let argvTweaked =
-        match argv with
-        | [ singleArg ] when not (singleArg.StartsWith("-")) ->
-            [ "--target"; singleArg ]
-        | _ -> argv
-    let execContext = Context.FakeExecutionContext.Create false "build.fsx" argvTweaked
-    Context.setExecutionContext (Fake.Core.Context.RuntimeContext.Fake execContext)
-
 [<EntryPoint>]
 let main argv =
-    setupFakeContext (List.ofArray argv)
+    BuildTask.setupContextFromArgv argv
     BuildServer.install [ AppVeyor.Installer ]
 
     let defaultTask = Tasks.createAndGetDefault ()
