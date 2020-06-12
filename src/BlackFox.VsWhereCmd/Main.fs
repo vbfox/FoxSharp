42afab8fcb2ce6a2f0375d6299a74a6eae50d065
Julien Roncaglia <julien@roncaglia.fr>
2020-06-12 23:26:52 +0200

    Add a small command line utility for VSWhere

---
+++ src/BlackFox.VsWhereCmd/Main.fs
@@ -0,0 +1,13 @@
+module BlackFox.VsWhereCmd
+
+open BlackFox.VsWhere
+
+[<EntryPoint>]
+let main _args =
+    for i in VsInstances.getAllWithLegacy() do
+        printfn "%s - %s" i.DisplayName i.InstallationVersion
+        printfn "    %s" i.InstallationPath
+        printfn ""
+
+
+    0
